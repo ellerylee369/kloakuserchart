43239fa6f46e7bcc8243f3a963dd762e89fed69c
Ellery <ellerywh@alumni.ubc.ca>
2020-05-26 16:40:10 -0700

    Revised some code in app.ts which will be reflected in app.js when compiled

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-var pgp = require('pg-promise')();
-var yargs = require('yargs');
-var db_connect = {
+//have to compile with tsc first, then run node app.js
+//in this format "node app.js <username> <password>"
+const pgp = require('pg-promise')();
+const yargs = require('yargs');
+const db_connect = {
     host: 'localhost',
     port: 5432,
     database: 'kloakuser',
-    user: 'postgres',
-    password: 'Flandrewaifu_1',
-    max: 30
+    user: process.argv[2],
+    password: process.argv[3],
+    max: 30,
 };
-var db = pgp(db_connect);
-var query = 'select date, number_of_users from totaluser';
-db.each(query, [], function (totaluser) {
-    var date = totaluser.date;
-    var number_of_users = totaluser.number_of_users;
-    console.log(date + ' - ' + number_of_users);
-})["catch"](function (error) { return console.error('database query failed', error); });
+const db = pgp(db_connect);
+let query = 'select date, number_of_users from totaluser';
+db.each(query, [], (totaluser) => {
+    const date = totaluser.date;
+    const date_number = date.getDate();
+    const date_month = date.getMonth();
+    const date_year = date.getFullYear();
+    let monthWords = [
+        "January", "February", "March", "April", "May", "June", "July",
+        "September", "October", "November", "December"
+    ];
+    const dateString = date_number + " " + monthWords[date_month] + " " + date_year;
+    const number_of_users = totaluser.number_of_users;
+    console.log(dateString + ' - ' + number_of_users);
+}).catch(error => console.error('database query failed. \n Must enter username and password with node arguments'));
--- app.ts
+++ app.ts
@@ -1,13 +1,15 @@
+//have to compile with tsc first, then run node app.js
+//in this format "node app.js <username> <password>"
+
 const pgp = require('pg-promise')()
 const yargs = require('yargs')
-
 
 const db_connect = {
     host: 'localhost',
     port: 5432,
     database: 'kloakuser',
-    user: 'postgres',
-    password: 'Flandrewaifu_1',
+    user: process.argv[2],
+    password: process.argv[3],
     max: 30,
     
 
@@ -19,7 +21,17 @@ let query = 'select date, number_of_users from totaluser'
 
 db.each(query, [], (totaluser:Totaluser) => {
     const date = totaluser.date
+    const date_number = date.getDate()
+    const date_month = date.getMonth()
+    const date_year = date.getFullYear()
+    let monthWords: string[] = [
+        "January", "February", "March", "April", "May", "June", "July",
+        "September", "October", "November", "December"
+    ]
+    const dateString = date_number + " " + monthWords[date_month] + " " + date_year
     const number_of_users = totaluser.number_of_users
-    console.log(date + ' - ' + number_of_users)
+    console.log(dateString + ' - ' + number_of_users)
 
-}).catch(error => console.error('database query failed', error))
+}).catch(error => console.error('database query failed. \n Must enter username and password with node arguments'))
+
+
