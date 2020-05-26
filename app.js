3cb8201e74edf3c19e3255dfe401677b3d83da59
Ellery <ellerywh@alumni.ubc.ca>
2020-05-26 13:32:21 -0700

    with compiler package added

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+exports.__esModule = true;
+var pgp = require('pg-promise')();
+var yargs = require('yargs');
+var db_connect = {
+    host: 'localhost',
+    port: 5432,
+    database: 'kloakuser',
+    user: 'postgres',
+    password: 'Flandrewaifu_1',
+    max: 30
+};
+var db = pgp(db_connect);
+var query = 'select date, number_of_users from totaluser';
+db.each(query, [], function (totaluser) {
+    var date = totaluser.date;
+    var number_of_users = totaluser.number_of_users;
+    console.log(date + ' - ' + number_of_users);
+})["catch"](function (error) { return console.error('database query failed', error); });
